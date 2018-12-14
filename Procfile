c00c55c0b3e9110f90fae293cc502c15d9f06e4d
Miheretab Alemu <mihrtab@gmail.com>
2018-12-15 02:30:59 +0300

    setting up vue

---
+++ Procfile
@@ -0,0 +1,2 @@
+backend: bin/rails s
+frontend: bin/webpack-dev-server
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from '../components/app.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.appendChild(document.createElement('app'))
+  const app = new Vue({
+    el: 'app',
+    template: '<App/>',
+    components: { App }
+  })
+
+  console.log(app)
+})
