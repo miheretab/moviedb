8f8791057d4039a6deb24c395a29dc8f861a732c
Miheretab Alemu <mihrtab@gmail.com>
2018-12-19 23:38:28 +0300

    Adding vue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
-import Vue from 'vue'
-//import App from '../components/app.vue'
+import Vue from 'vue/dist/vue.js'
+import App from '../components/app.vue'
 import StarRating from 'vue-star-rating'
 
 
