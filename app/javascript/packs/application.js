7e6a23a6c0e70a83f4f82e52e674b2b9098f72d3
Miheretab Alemu <mihrtab@gmail.com>
2018-12-19 06:09:16 +0300

    adding javascript js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,33 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import Vue from 'vue'
+import App from '../components/app.vue'
+import StarRating from 'vue-star-rating'
 
-console.log('Hello World from Webpacker')
+
+document.addEventListener('DOMContentLoaded', () => {
+  /*document.body.appendChild(document.createElement('app'))
+  const app = new Vue({
+    el: 'app',
+    template: '<App/>',
+    components: { App }
+  })
+
+  console.log(app)*/
+
+  Vue.component('star-rating', StarRating);
+
+  var i = 0
+  while(i <= 5) {
+    new Vue({
+        el: 'star-rating[data-score="'+i+'"]',
+        methods: {
+            setRating: function(rating){
+              this.rating= rating;
+            }
+        },
+        data: {
+            rating: i
+        }
+    });
+    i++;
+  }
+})
